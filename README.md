c165f11151b81b093b27b00e5941bf435062ae75
chris48s <c.mshaw@yahoo.co.uk>
2016-08-27 18:59:07 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+Scrape polling station and district data from Tonbridge and Malling Borough Council
+
+This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
---
+++ scraper.py
@@ -0,0 +1,10 @@
+from arcgis_scraper import scrape
+
+
+stations_url = "https://maps.tmbc.gov.uk/arcgis/rest/services/LocalViewPublic/Polling_Stations/MapServer/0/query?where=OBJECTID+LIKE+%27%25%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson"
+districts_url = "https://maps.tmbc.gov.uk/arcgis/rest/services/LocalViewPublic/Polling_Stations/MapServer/1/query?where=OBJECTID+LIKE+%27%25%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&f=pjson"
+council_id = 'E07000115'
+
+
+scrape(stations_url, council_id, 'utf-8', 'stations')
+scrape(districts_url, council_id, 'utf-8', 'districts')
